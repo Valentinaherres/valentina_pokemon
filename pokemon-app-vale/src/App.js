51932082e85d655ed60dd8927ec227ab4f189796
Valentinaherres <vherress@gmail.com>
2024-06-23 21:29:16 -0600

    Change in HasRouter and RouterDom

--- pokemon-app-vale/src/App.js
+++ pokemon-app-vale/src/App.js
@@ -35,7 +35,7 @@
 
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { PokemonProvider } from './context/PokemonContext';
 import PokemonList from './components/PokemonList';
 import PokemonDetails from './components/PokemonDetails';
